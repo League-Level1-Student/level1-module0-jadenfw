df03a8d9ee8a4904ce4d7f8c4904df882c6af65f
League Student <league@iMac-16.attlocal.net>
2019-08-09 16:23:50 -0700

    houses and myfirstswing

---
+++ src/_01_houses/houses.java
@@ -0,0 +1,120 @@
+package _01_houses;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class houses {
+//	Robot rob = new Robot();
+// void house() {
+//		
+//		rob.setSpeed(10);
+//		rob.moveTo(10, 500);
+//		rob.penDown();
+//		rob.setPenWidth(5); 
+//	 for (int i = 0; i < 10; i++)
+//	 {
+//	Random ran = new Random();
+//	int r = ran.nextInt(50)+50;
+//	Random rann = new Random();
+//	int r2 = rann.nextInt(255);
+//	Random rannn = new Random();
+//	int r3 = rannn.nextInt(3);
+//	Random rannnn = new Random();
+//	int r4 = rannnn.nextInt(2);
+//	if(r3 == 1)
+//	{
+//		rob.setPenColor(r2, 0, 0);
+//	}
+//	if(r3 == 2)
+//	{
+//		rob.setPenColor(0, r2, 0);
+//	}
+//	if(r3 == 3)
+//	{
+//		rob.setPenColor(0, 0, r2);
+//	}
+//	rob.move(r);
+//	if(r4==1)
+//	{
+//		rob.turn(90);
+//		rob.move(20);
+//		rob.turn(90);
+//	}
+//	else
+//	{
+//		rob.turn(45);
+//		rob.move(10);
+//		rob.turn(90);
+//		rob.move(10);
+//		rob.turn(45);
+//	}
+//	rob.move(r);
+//	rob.turn(-90);
+//	rob.setPenColor(0, 255, 0);
+//	rob.move(20);
+//	rob.turn(-90);
+//	
+//	
+//	 }
+//	 rob.hide();
+//	}*/
+public static void main(String[] args) {
+	
+//houses h= new houses();
+//h.house();
+	 
+Robot rob = new Robot();
+rob.setSpeed(10);
+rob.moveTo(10, 500);
+rob.penDown();
+rob.setPenWidth(5);
+for (int i = 0; i < 10; i++)
+{
+Random ran = new Random();
+int r = ran.nextInt(50)+50;
+Random rr = new Random();
+int r2 = rr.nextInt(255);
+Random rann = new Random();
+int r3 = rann.nextInt(3);
+Random rannn = new Random();
+int r4 = rannn.nextInt(2);
+rob.setPenColor(r2, 0, 0);
+
+if(r3 == 1)
+{
+	rob.setPenColor(r2, 0, 0);
+}
+else if(r3 == 2)
+{
+	rob.setPenColor(0, r2, 0);
+}
+else
+{
+	rob.setPenColor(0, 0, r2);
+}
+rob.move(r);
+if(r4 == 1)
+{
+rob.turn(90);
+rob.move(20);
+rob.turn(90);
+}
+else
+{
+rob.turn(45);
+rob.move(10);
+rob.turn(90);
+rob.move(10);
+rob.turn(45);
+}
+rob.move(r);
+rob.turn(-90);
+rob.setPenColor(0, 255, 0);
+rob.move(20);
+rob.turn(-90);
+
+}	
+rob.hide();	
+
+}
+}
--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,34 +16,34 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Create and initialize an object of the JFrame class
-
+		JFrame jf = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+		jf.setVisible(true);
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+	JPanel jp = new JPanel(); 
 		// 6. Create and initialize an object of the JLabel class
-
+		JLabel jl = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+		jl.setText("Hi how are you?");
 		// 8. Add the JPanel object to the JFrame
-
+		jf.add(jp);
 		// 9. Add the JLabel object to the JPanel
-
+		jp.add(jl);
 		// 10. Pack your JFrame.
-
+		jf.pack();
 		// 11. Run your program again. Do you see your message.
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+		jl.setIcon(loadImage());
 		// 13. Re-pack the JFrame object.
-
+		jf.pack();
 		// 14. Run the program one more time. Do you see the image?
-
+		System.out.print("3");
 	}
 
 	public ImageIcon loadImage() {
